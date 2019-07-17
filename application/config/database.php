c167c35f487c7d2de203551f4ed94009d9513c68
Jonathan Yombo <jonathanyombo@gmail.com>
2019-07-17 23:16:49 +0200

    [CONFIG] all config done...
    Signed-off-by: Jonathan Yombo <jonathanyombo@gmail.com>

--- application/config/database.php
+++ application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'mysql:hostname=localhost;dbname=laure',
 	'hostname' => 'localhost',
-	'username' => '',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
+	'database' => 'laure',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
