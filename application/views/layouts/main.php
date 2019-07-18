0cfb3d39c719f0ca7d13c341a723040b08ab0899
Jonathan Yombo <jonathanyombo@gmail.com>
2019-07-18 12:06:23 +0200

    [UPDATE] add select2 css/js
    Signed-off-by: Jonathan Yombo <jonathanyombo@gmail.com>

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -12,13 +12,13 @@
         <link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.min.css');?>">
         <!-- Ionicons -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-        <!-- Datetimepicker -->
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap-datetimepicker.min.css');?>">
         <!-- Theme style -->
         <link rel="stylesheet" href="<?php echo base_url('assets/css/AdminLTE.min.css');?>">
         <!-- AdminLTE Skins. Choose a skin from the css/skins
              folder instead of downloading all of them to reduce the load. -->
         <link rel="stylesheet" href="<?php echo base_url('assets/css/_all-skins.min.css');?>">
+
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/select2.min.css');?>">
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -76,7 +76,7 @@
                                             <a href="<?= site_url('user/profil') ?>" class="btn btn-default btn-flat">Profil</a>
                                         </div>
                                         <div class="pull-right">
-                                            <a href="<?= site_url('user/logout') ?>" class="btn btn-default btn-flat">Déconnexion</a>
+                                            <a href="<?= site_url('auth/logout') ?>" class="btn btn-default btn-flat">Déconnexion</a>
                                         </div>
                                     </li>
                                 </ul>
@@ -158,7 +158,7 @@
 						</div>
 					<?php endif ?>
 					<h1>
-						<?= isset($title) ? $title : '' ?>
+						<?= isset($title) ? ucfirst($title) : APP_NAME ?>
 						<small><?= isset($desc) ? $desc : '' ?></small>
 					</h1>
 				</section>
@@ -210,11 +210,12 @@
         <script src="<?php echo base_url('assets/js/fastclick.js');?>"></script>
         <!-- AdminLTE App -->
         <script src="<?php echo base_url('assets/js/app.min.js');?>"></script>
-        <!-- AdminLTE for demo purposes -->
-        <script src="<?php echo base_url('assets/js/demo.js');?>"></script>
-        <!-- DatePicker -->
-        <script src="<?php echo base_url('assets/js/moment.js');?>"></script>
-        <script src="<?php echo base_url('assets/js/bootstrap-datetimepicker.min.js');?>"></script>
-        <script src="<?php echo base_url('assets/js/global.js');?>"></script>
+
+        <script src="<?php echo base_url('assets/js/select2.min.js');?>"></script>
+		<script>
+			$(function () {
+				$('.select2').select2();
+			});
+		</script>
     </body>
 </html>
