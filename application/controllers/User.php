66c61329dab0adda57dc4b65365bf57a549c617d
Jonathan Yombo <jonathanyombo@gmail.com>
2019-07-18 02:12:24 +0200

    [ADD] user controller
    Signed-off-by: Jonathan Yombo <jonathanyombo@gmail.com>

---
+++ application/controllers/User.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		if (!isset($this->session->is_connected)){
+			redirect('auth/index');
+		}
+
+		$this->load->model('users');
+	}
+
+	public function index()
+	{
+		if ($this->session->level != 2){
+			redirect('archive');
+		}
+
+		$this->load->helper('date');
+		$params['limit'] = RECORDS_PER_PAGE;
+		$params['offset'] = ($this->input->get('per_page')) ? $this->input->get('per_page') : 0;
+
+		$config = $this->config->item('pagination');
+		$config['base_url'] = site_url('user/index?');
+		$config['total_rows'] = $this->users->get_all_users_count();
+		$this->pagination->initialize($config);
+
+		$data['users'] = $this->users->get_all_users($params);
+
+		$data['_view'] = $this->load->view('users/index', $data, true);
+		$this->load->view('layouts/main',$data);
+	}
+	function logout()
+	{
+		$this->session->unset_userdata('is_connected');
+		redirect();
+	}
+}
